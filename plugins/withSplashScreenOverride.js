abe9fe3690eaf89ef48aa4a9025f6732201bd849
Vishal-gsu <vishalgsu@gmail.com>
2025-09-15 12:42:25 +0530

    fix(android): Address manifest merger conflict and refactor plugin

--- plugins/withSplashScreenOverride.js
+++ plugins/withSplashScreenOverride.js
@@ -1,8 +1,9 @@
 // plugins/withSplashScreenOverride.js
 
-const { withAppBuildGradle } = require('@expo/config-plugins');
+const { withAppBuildGradle, withAndroidManifest } = require('@expo/config-plugins');
 
-module.exports = function withSplashScreenOverride(config) {
+// Fix for the splash screen dependency
+function withSplashScreenGradle(config) {
   return withAppBuildGradle(config, (config) => {
     const { modResults } = config;
     const { contents } = modResults;
@@ -17,9 +18,32 @@ module.exports = function withSplashScreenOverride(config) {
       }
     `;
 
-    // Add the resolutionStrategy block at the end of the file
     modResults.contents = contents + resolutionStrategy;
     
     return config;
   });
+}
+
+// Fix for the manifest merger conflict
+function withManifestTools(config) {
+  return withAndroidManifest(config, (config) => {
+    const { modResults } = config;
+    const mainApplication = modResults.manifest.application[0];
+
+    if (!mainApplication.$) {
+      mainApplication.$ = {};
+    }
+    
+    // Add 'tools:replace="android:appComponentFactory"' to the <application> tag
+    mainApplication.$['tools:replace'] = 'android:appComponentFactory';
+
+    return config;
+  });
+}
+
+// Chain the plugins together
+module.exports = (config) => {
+  config = withSplashScreenGradle(config);
+  config = withManifestTools(config);
+  return config;
 };
